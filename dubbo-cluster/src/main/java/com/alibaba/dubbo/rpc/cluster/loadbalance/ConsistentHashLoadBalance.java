22f1a5513891d432e883210bcb1852b8ae6f154e
JackCaptain1015 <15700094730@163.com>
2018-03-31 23:06:58 +0800

    学习注释(ConsistentHashLoadBalance)

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
@@ -41,12 +41,21 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
     @SuppressWarnings("unchecked")
     @Override
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
-        //group/interface:version.method
+        /**
+         * group/interface:version.method
+         * 无论provider部署了多少个，但是调用的方法还是一样的
+         */
         String key = invokers.get(0).getUrl().getServiceKey() + "." + invocation.getMethodName();
         //生成调用列表hashCode
         int identityHashCode = System.identityHashCode(invokers);
+        /**
+         * (缓存)得到每个方法对应的ConsistentHashSelector
+         */
         ConsistentHashSelector<T> selector = (ConsistentHashSelector<T>) selectors.get(key);
         if (selector == null || selector.getIdentityHashCode() != identityHashCode) {
+            /**
+             * 得到每个invoker的虚拟节点
+             */
             selectors.put(key, new ConsistentHashSelector<T>(invokers, invocation.getMethodName(), identityHashCode));
             selector = (ConsistentHashSelector<T>) selectors.get(key);
         }
@@ -64,6 +73,9 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         private final int[]                     argumentIndex;
 
         public ConsistentHashSelector(List<Invoker<T>> invokers, String methodName, int identityHashCode) {
+            /**
+             * 虚拟节点为红黑树，key为hash值，value为Invoker
+             */
             this.virtualInvokers = new TreeMap<Long, Invoker<T>>();
             this.identityHashCode = identityHashCode;
             // 获取url，比如：dubbo://169.254.90.37:20880/service.DemoService?
@@ -78,8 +90,14 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
             for (int i = 0; i < index.length; i ++) {
                 argumentIndex[i] = Integer.parseInt(index[i]);
             }
+            /**
+             * 每个invoker都有replicaNumber/4个备份节点，
+             * 其中每个备份节点都通过md5后被hash分成4段散开，
+             * 所以每个invoker共有replicaNumber个虚拟节点
+             */
             for (Invoker<T> invoker : invokers) {
                 for (int i = 0; i < replicaNumber / 4; i++) {
+
                     byte[] digest = md5(invoker.getUrl().toFullString() + i);
                     for (int h = 0; h < 4; h++) {
                         long m = hash(digest, h);
@@ -96,10 +114,17 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         public Invoker<T> select(Invocation invocation) {
             String key = toKey(invocation.getArguments());
             byte[] digest = md5(key);
+            /**
+             * 取参数的md5来得到第一段的hash值，
+             * (如果存在)那么就直接返回invoker
+             */
             Invoker<T> invoker = sekectForKey(hash(digest, 0));
             return invoker;
         }
 
+        /**
+         * 拼接参数
+         */
         private String toKey(Object[] args) {
             StringBuilder buf = new StringBuilder();
             for (int i : argumentIndex) {
@@ -114,6 +139,11 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
             Invoker<T> invoker;
             Long key = hash;
             if (!virtualInvokers.containsKey(key)) {
+                /**
+                 * 得到虚拟节点中hash值大于等于key的部分,
+                 * 如果tailMap是空就取虚拟节点第一个，
+                 * 否则就取该hash值的下一个节点（即转移到下一个节点上）
+                 */
                 SortedMap<Long, Invoker<T>> tailMap = virtualInvokers.tailMap(key);
                 if (tailMap.isEmpty()) {
                     key = virtualInvokers.firstKey();
@@ -125,11 +155,17 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
             return invoker;
         }
 
+        /**
+         * 该函数将生成的结果转换为long类，这是因为生成的结果是一个32位数，
+         * 若用int保存可能会产生负数。
+         * 而一致性hash生成的逻辑环其hashCode的范围是在 0 - MAX_VALUE之间。
+         * 因此为正整数，所以这里要强制转换为long类型，避免出现负数。
+         */
         private long hash(byte[] digest, int number) {
             return (((long) (digest[3 + number * 4] & 0xFF) << 24)
                     | ((long) (digest[2 + number * 4] & 0xFF) << 16)
-                    | ((long) (digest[1 + number * 4] & 0xFF) << 8) 
-                    | (digest[0 + number * 4] & 0xFF)) 
+                    | ((long) (digest[1 + number * 4] & 0xFF) << 8)
+                    | (digest[0 + number * 4] & 0xFF))
                     & 0xFFFFFFFFL;
         }
 
